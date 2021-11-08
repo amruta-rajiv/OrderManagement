3bef1624fa397090ff857f13bcea325ec41b6b2a
amruta-rajiv <93946198+amruta-rajiv@users.noreply.github.com>
2021-11-09 09:59:43 +1300

    Improvements
    Separate classes for constants and helpers

---
+++ Order.Management/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Management
+{
+    public class Constants
+    {
+        public static int SquareUnitPrice = 1;
+        public static int TriangleUnitPrice = 2;
+        public static int CircleUnitPrice = 3;
+        public static List<string> ShapeType = new List<string> { "Squares", "Triangles", "Circles" };
+        public static List<string> ShapeColours = new List<string> { "Red", "Blue", "Yellow" };
+        public static int InvoiceReportTableWidth = 73;
+        public static int CuttingListReportTableWidth = 20;
+        public static int PaintingReportTableWidth = 73;
+    }
+}
---
+++ Order.Management/Helpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Order.Management
+{
+    public class Helpers
+    {
+        public static string GetStringInput()
+        {
+            string input = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Please enter valid details.");
+                input = Console.ReadLine();
+            }
+
+            return input;
+        }
+
+        public static int GetNumberInput()
+        {
+            int number;
+            string input = Console.ReadLine();
+
+            while (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                input = Console.ReadLine();
+            }
+
+            return number;
+        }
+
+        public static DateTime GetDateInput()
+        {
+            DateTime input;
+            string strInput = Console.ReadLine();
+
+            while (!DateTime.TryParse(strInput, out input))
+            {
+                Console.WriteLine("Please enter a valid date.");
+                strInput = Console.ReadLine();
+            }
+            return input;
+        }
+    }
+}
