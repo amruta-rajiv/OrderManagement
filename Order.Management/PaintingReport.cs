e6a159a2eee2946b4f90f7649380724a90f96c8a
amruta-rajiv <93946198+amruta-rajiv@users.noreply.github.com>
2021-11-09 10:02:48 +1300

    improvements

--- Order.Management/PaintingReport.cs
+++ Order.Management/PaintingReport.cs
@@ -1,67 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Order.Management
 {
-    class PaintingReport : Order
+    class PaintingReport : ReportHelper
     {
-        public int tableWidth = 73;
-        public PaintingReport(string customerName, string customerAddress, string dueDate, List<Shape> shapes)
+        public PaintingReport(string customerName, string customerAddress, DateTime dueDate, List<Shape> shapes)
         {
-            base.CustomerName = customerName;
-            base.Address = customerAddress;
-            base.DueDate = dueDate;
-            base.OrderedBlocks = shapes;
+            CustomerName = customerName;
+            Address = customerAddress;
+            DueDate = dueDate;
+            OrderedBlocks = shapes;
+            tableWidth = Constants.PaintingReportTableWidth;
         }
+
         public override void GenerateReport()
         {
-            Console.WriteLine("\nYour painting report has been generated: ");
-            Console.WriteLine(base.ToString());
+            Console.WriteLine($"\nYour painting report has been generated: \n{OrderDescription()}");
             generateTable();
         }
 
-        public void generateTable()
+        void generateTable()
         {
             PrintLine();
             PrintRow("        ", "   Red   ", "  Blue  ", " Yellow ");
             PrintLine();
-            PrintRow("Square", base.OrderedBlocks[0].NumberOfRedShape.ToString(), base.OrderedBlocks[0].NumberOfBlueShape.ToString(), base.OrderedBlocks[0].NumberOfYellowShape.ToString());
-            PrintRow("Triangle", base.OrderedBlocks[1].NumberOfRedShape.ToString(), base.OrderedBlocks[1].NumberOfBlueShape.ToString(), base.OrderedBlocks[1].NumberOfYellowShape.ToString());
-            PrintRow("Circle", base.OrderedBlocks[2].NumberOfRedShape.ToString(), base.OrderedBlocks[2].NumberOfBlueShape.ToString(), base.OrderedBlocks[2].NumberOfYellowShape.ToString());
+
+            foreach (var shape in OrderedBlocks)
+                PrintRow(shape.Name, shape.NumberOfRedShapes.ToString(), shape.NumberOfBlueShapes.ToString(), shape.NumberOfYellowShapes.ToString());
+
             PrintLine();
-        }
-       
-        public void PrintLine()
-        {
-            Console.WriteLine(new string('-', tableWidth));
-        }
-
-        public void PrintRow(params string[] columns)
-        {
-            int width = (tableWidth - columns.Length) / columns.Length;
-            string row = "|";
-
-            foreach (string column in columns)
-            {
-                row += AlignCentre(column, width) + "|";
-            }
-
-            Console.WriteLine(row);
-        }
-
-        public string AlignCentre(string text, int width)
-        {
-            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
-
-            if (string.IsNullOrEmpty(text))
-            {
-                return new string(' ', width);
-            }
-            else
-            {
-                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
-            }
         }
     }
 }
